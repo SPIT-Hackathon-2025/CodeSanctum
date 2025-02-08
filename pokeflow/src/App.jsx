e60aceec70a2b478519891194d2542e77861090d
NarveshRaju <narveshraju@gmail.com>
2025-02-08 14:14:00 +0530

    helo

--- pokeflow/src/App.jsx
+++ pokeflow/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
+import './App.css';
+import SignUpPage from './sign-up';
+import Home from './home';
+import SignInPage from "./Signin" 
+// import Workflow from './workflow';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function NotFound() {
+  return <h1>404 - Page Not Found</h1>;
+}
+function RedirectAfterSignIn() {
+  const navigate = useNavigate();
+  navigate('/about');
+  return null;
 }
 
-export default App
+function App() {
+  return (
+    <Router>
+      <SignedOut>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignInPage/>} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="*" element={<Navigate to="/sign-in" />} />
+        </Routes>
+      </SignedOut>
+
+      <SignedIn>
+        <RedirectAfterSignIn />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<>hello</>} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </SignedIn>
+    </Router>
+  );
+}
+
+export default App;
